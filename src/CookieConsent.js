6b7da00cabc3d92b2c7342fd967936a9d2af789e
John Brynte <john.brynte@gmail.com>
2021-04-29 23:55:44 +0200

    Cookie consent render fix

--- src/CookieConsent.js
+++ src/CookieConsent.js
@@ -6,24 +6,30 @@ import styles from "./CookieConsent.module.css";
 const CookieConsent = () => {
     const cookieString = "indie.johnbrynte.harjagpollenallergi.consent";
 
-    const [cookieConsent, setCookieConsent] = useState(false);
+    // default true, to avoid the element being rendered before checking
+    const [cookieConsent, setCookieConsent] = useState(true);
 
     useEffect(() => {
         try {
             const consent = window.localStorage.getItem(cookieString);
             if (consent) {
                 setCookieConsent(consent === "true");
+            } else {
+                // remember to set it to false here
+                setCookieConsent(false);
             }
         } catch (e) { }
     }, []);
 
-    useEffect(() => {
-        window.localStorage.setItem(cookieString, cookieConsent);
-    }, [cookieConsent]);
+    const acceptCookieTerms = () => {
+        setCookieConsent(true);
+
+        window.localStorage.setItem(cookieString, true);
+    }
 
     return !cookieConsent && (
         <div>
-            <div className={styles.info}>Sidan använder "cookies" för att lagra den data du matar in. <Button compact click={() => setCookieConsent(true)}>OK</Button></div>
+            <div className={styles.info}>Sidan använder "cookies" för att lagra den data du matar in. <Button compact click={acceptCookieTerms}>OK</Button></div>
         </div>
     );
 };
